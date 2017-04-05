0f64cede57543c855aab9a934ab6c434cd8d44b1
eight <eight04@gmail.com>
2017-04-05 12:39:17 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+巴哈姆特哈拉開圖器 edited by eight
+===============================
+
+由 [巴哈姆特哈拉開圖器](http://userscripts-mirror.org/scripts/review/142033) by akiratw 修改而來。
+
+> * 在未登入時也可以直接顯示 [巴哈姆特哈啦區](http://forum.gamer.com.tw/) 文章裡的圖片和 YouTube 影片。
+> * 跳過巴哈姆特的站外連結警告頁面。
+
+修正
+----
+* 加入圖片並行載入限制，最多同時載入 3 張圖片
+* 其它小修正
+
+Install
+-------
+
+License
+-------
+在 userscript 內標示為 MIT，然而我找不到原 License 檔案。
+
+Changelog
+---------
+* Next
+	- First release.
---
+++ baha-embed-images.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name        巴哈姆特哈拉開圖器 edited by eight
+// @description	直接顯示巴哈姆特哈啦區文章的圖片和影片。跳過站外連結警告。
+// @namespace   eight04.blogspot.com
+// @include     http://forum.gamer.com.tw/*
+// @include     https://forum.gamer.com.tw/*
+// @version     1.1.1
+// @author		akiratw
+// @homepage	https://github.com/eight04/baha-embed-images
+// @supportURL	https://github.com/eight04/baha-embed-images/issues
+// @license		MIT
+// @compatible	firefox
+// @compatible	chrome
+// @grant		none
+// ==/UserScript==
+
+var redirectLinks = function () {
+    var links = document.querySelectorAll('a'),
+        regex = /.*redir\.php\?url=(.+)/i;
+
+    for (var i = links.length - 1; i >= 0; i--) {
+        var link = links[i],
+            url = link.getAttribute('href');
+
+        if (regex.test(url)) {
+            link.setAttribute(
+                'href',
+                decodeURIComponent(url.replace(regex, '$1'))
+            );
+        }
+    }
+}();
+
+imageEmbeder(document.querySelectorAll('.FM-cbox7 a'), 3);
+
+var displayYouTube = function () {
+    var links = document.querySelectorAll('a.T3_red'),
+        regex = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/i;
+
+    for (var i = links.length - 1; i >= 0; i--) {
+        var link = links[i],
+            url = link.getAttribute('href'),
+            matches = url.match(regex);
+
+        if (matches) {
+            url = 'http://www.youtube.com/embed/' + matches[1];
+            var iframe = document.createElement('iframe');
+            iframe.setAttribute('width', '560');
+            iframe.setAttribute('height', '315');
+            iframe.setAttribute('frameborder', '0');
+            iframe.setAttribute('allowfullscreen', 'true');
+            iframe.setAttribute('src', url);
+            link.parentNode.replaceChild(iframe, link);
+        }
+    }
+}();
+
+function imageEmbeder(links, limit) {
+	links = Array.from(links);
+	
+	while (limit--) {
+		deque();
+	}
+	
+	function deque() {
+		var link = links.shift();
+		
+		if (!link) return;
+		
+        var src = link.id || link.href;
+            
+		if (!/(\.gif|\.jpg|\.jpeg|\.png|\.bmp)/i.test(src) || !link.textContent.includes("圖片")) {
+			return deque();
+		}
+		
+		var img = new Image;
+		img.src = src;
+		img.addEventListener("load", handleLoad);
+		img.addEventListener("error", handleLoad);
+		
+		link.innerHTML = "";
+		link.onclick = null;
+		link.target = "_blank";
+		link.appendChild(img);
+	}
+	
+	function handleLoad(e) {
+		e.target.removeEventListener("load", handleLoad);
+		e.target.removeEventListener("error", handleLoad);
+		return deque();
+	}
+}
