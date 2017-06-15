d1cbfb1c8ab43f465af89563a61b97064493ad54
eight <eight04@gmail.com>
2017-06-15 09:04:58 +0800

    Release v1.2.1

--- baha-embed-images.user.js
+++ baha-embed-images.user.js
@@ -4,7 +4,7 @@
 // @namespace   eight04.blogspot.com
 // @include     http://forum.gamer.com.tw/*
 // @include     https://forum.gamer.com.tw/*
-// @version     1.2.0
+// @version     1.2.1
 // @author		akiratw
 // @contributor	eight04 <eight04@gmail.com> (https://github.com/eight04)
 // @homepage	https://github.com/eight04/baha-embed-images
